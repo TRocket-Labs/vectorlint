c5c96253623c7ab6a2d4896202fc2373308dc177
oluwatooki-GA <oluwatooki.g.adegbite@gmail.com>
2025-11-07 10:34:14 +0100

    fix: remove use of AzureOpenAIProvider and use createProvider

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 import { program } from 'commander';
 import { readFileSync, existsSync } from 'fs';
 import * as path from 'path';
-import { AzureOpenAIProvider } from './providers/azure-openai-provider';
+import { createProvider } from './providers/provider-factory';
 import { PerplexitySearchProvider } from './providers/perplexity-provider';
 import { loadConfig } from './boundaries/config-loader';
 import { loadPrompts, type PromptFile } from './prompts/prompt-loader';
@@ -168,17 +168,13 @@ program
       process.exit(1);
     }
     
-    const provider = new AzureOpenAIProvider({
-      apiKey: env.AZURE_OPENAI_API_KEY,
-      endpoint: env.AZURE_OPENAI_ENDPOINT,
-      deploymentName: env.AZURE_OPENAI_DEPLOYMENT_NAME,
-      apiVersion: env.AZURE_OPENAI_API_VERSION,
-      temperature: env.AZURE_OPENAI_TEMPERATURE,
+        const provider = createProvider(env, {
       debug: cliOptions.verbose,
       showPrompt: cliOptions.showPrompt,
       showPromptTrunc: cliOptions.showPromptTrunc,
       debugJson: cliOptions.debugJson,
     }, new DefaultRequestBuilder(directive));
+    
     
     if (cliOptions.verbose) {
       const directiveLen = directive ? directive.length : 0;
